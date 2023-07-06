1034e1ac30f7f6e335b8b37b6e8df3c32989a201
Rameesha Khan <rameeshakhan75@gmail.com>
2023-07-06 20:29:13 +0500

    Respsivness Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,89 +1,124 @@
-import React from 'react';
-import { Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Typography, Button, Drawer, List, ListItem, ListItemText, IconButton, useMediaQuery } from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
 import logo from "../assets/images/logo.png";
-import deepDuck from "../assets/images/deepDuck.png";
 import { useNavigate } from 'react-router-dom';
 
-const Navbar = () => {
-const navigate = useNavigate()
+const Navbar = ( {deepDuckImage, optionColor}) => {
+
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const isMobile = useMediaQuery('(max-width: 650px)'); 
+
+  const handleToggle = () => {
+    setOpen(!open);
+  };
 
   return (
     <div style={{ display: 'flex', alignItems: 'center', padding: '20px', justifyContent: 'space-between' }}>
-      {/* Images Section */}
+
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <picture>
           <img src={logo} alt="Logo" height="39px" width="47px" />
         </picture>
         <span style={{ marginLeft: '10px' }}>
-          <img src={deepDuck} alt="Logo" height="17px" width="86px" />
+          <img src={deepDuckImage} alt="Logo" height="17px" width="86px" />
         </span>
       </div>
 
-      {/* Typography Section */}
-      <div style={{ flex: '1', textAlign: 'center' }}>
-        <Typography variant="p" style={{ fontFamily: 'Raleway', fontSize: '14px', fontWeight: 600, lineHeight: '16px', letterSpacing: '0em', textAlign: 'left', color: '#888888', margin: "0px 20px" }}>
-          Swap it
-        </Typography>
-        <Typography variant="p" style={{ fontFamily: 'Raleway', fontSize: '14px', fontWeight: 600, lineHeight: '16px', letterSpacing: '0em', textAlign: 'left', color: '#888888' ,margin: "0px 20px" }}>
-          Feature
-        </Typography>
-        <Typography variant="p" style={{ fontFamily: 'Raleway', fontSize: '14px', fontWeight: 600, lineHeight: '16px', letterSpacing: '0em', textAlign: 'left', color: '#888888', margin: "0px 20px"  }}>
-          Pricing
-        </Typography>
-      </div>
+      {isMobile ? (
+        <>
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleToggle}>
+            <MenuIcon sx={{color: optionColor}}/>
+          </IconButton>
 
-      {/* Buttons Section */}
-      <div>
-      <Button
-        variant="contained"
-        sx={{
-          margin: '5px',
-          background: 'none',
-          outline: 'none',
-          fontFamily: 'Raleway',
-          fontSize: '14px',
-          fontWeight: 600,
-          lineHeight: '16px',
-          letterSpacing: '0em',
-          textAlign: 'left',
-          textTransform: 'none',
-          border: "none",
-          boxShadow: "none",
-          '&:hover': {
-            background: 'none',
-            color: 'inherit',
-            outline: 'none',
-          },
-        }}
-        onClick={() => navigate('/login')}
-      >
-        Log In
-      </Button>
-      <Button
-        variant="contained"
-        sx={{
-          background: 'rgba(255, 214, 0, 1)',
-          outline: 'none',
-          color: 'white',
-          fontFamily: 'Raleway',
-          fontSize: '14px',
-          fontWeight: 600,
-          lineHeight: '16px',
-          letterSpacing: '0em',
-          textAlign: 'left',
-          borderRadius: '8px',
-          textTransform: 'none',
-          '&:hover': {
-            background: 'rgba(255, 214, 0, 1)',
-            color: 'white',
-            outline: 'none',
-          },
-        }}
-        onClick={() => navigate('/signup')}
-      >
-        Sign Up
-      </Button>
-      </div>
+          <Drawer anchor="right" open={open} onClose={handleToggle} PaperProps={{ sx: { backgroundColor: 'black', color: 'white' } }}>
+            <List style={{ width: '200px' }}>
+              <ListItem button onClick={() => navigate('/login')}>
+                <ListItemText primary="Log In" />
+              </ListItem>
+              <ListItem button onClick={() => navigate('/signup')}>
+                <ListItemText primary="Sign Up" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Swap it" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Feature" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Pricing" />
+              </ListItem>
+            </List>
+          </Drawer>
+        </>
+      ) : (
+        <>
+          <div style={{ flex: '1', textAlign: 'center' }}>
+        <Typography variant="p" style={{ fontFamily: 'Raleway', fontSize: '14px', fontWeight: 600, lineHeight: '16px', letterSpacing: '0em', textAlign: 'left', color: optionColor, margin: "0px 20px" }}>
+        Swap it
+      </Typography>
+      <Typography variant="p" style={{ fontFamily: 'Raleway', fontSize: '14px', fontWeight: 600, lineHeight: '16px', letterSpacing: '0em', textAlign: 'left', color: optionColor, margin: "0px 20px" }}>
+        Feature
+      </Typography>
+      <Typography variant="p" style={{ fontFamily: 'Raleway', fontSize: '14px', fontWeight: 600, lineHeight: '16px', letterSpacing: '0em', textAlign: 'left', color: optionColor, margin: "0px 20px"  }}>
+        Pricing
+      </Typography>
+          </div>
+          <div>
+            <Button
+              variant="contained"
+              sx={{
+                color: optionColor,
+                margin: '5px',
+                background: 'none',
+                outline: 'none',
+                fontFamily: 'Raleway',
+                fontSize: '14px',
+                fontWeight: 600,
+                lineHeight: '16px',
+                letterSpacing: '0em',
+                textAlign: 'left',
+                textTransform: 'none',
+                border: "none",
+                boxShadow: "none",
+                '&:hover': {
+                  background: 'none',
+                  color: 'inherit',
+                  outline: 'none',
+                },
+              }}
+              onClick={() => navigate('/login')}
+            >
+              Log In
+            </Button>
+            <Button
+              variant="contained"
+              sx={{
+                background: 'rgba(255, 214, 0, 1)',
+                outline: 'none',
+                fontFamily: 'Raleway',
+                fontSize: '14px',
+                fontWeight: 600,
+                lineHeight: '16px',
+                letterSpacing: '0em',
+                textAlign: 'left',
+                borderRadius: '8px',
+                textTransform: 'none',
+                color: {optionColor},
+                '&:hover': {
+                  background: 'rgba(255, 214, 0, 1)',
+                  color: 'white',
+                  outline: 'none',
+                },
+              }}
+              onClick={() => navigate('/signup')}
+            >
+              Sign Up
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
