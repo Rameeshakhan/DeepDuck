0fc53bf6ac580307a5108d8f45818e7f559bce00
Rameesha Khan <rameeshakhan75@gmail.com>
2023-07-06 18:36:28 +0500

    Routes Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing/Landing'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
+import ImageSwap from './pages/imageSwap/ImageSwap'
+import VideoSwap from './pages/videoSwap/VideoSwap'
+import GifSwap from './pages/gifSwap/GifSwap'
+import Policy from './pages/Policy/Policy'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Landing/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/imageswap" element={<ImageSwap />} />
+          <Route path="/videoswap" element={<VideoSwap />} />
+          <Route path="/gifswap" element={<GifSwap />} />
+          <Route path="/policy" element={<Policy />} />
+        </Routes>
       </BrowserRouter>
     </div>
   )
