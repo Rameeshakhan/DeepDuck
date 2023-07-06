d5639ea331078813223ba6340ab0409221593576
Rameesha Khan <rameeshakhan75@gmail.com>
2023-07-06 20:28:34 +0500

    Modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BiSolidPhone, BiLogoWhatsapp } from 'react-icons/bi';
 import { BsFacebook } from "react-icons/bs"
 import { HiOutlineMail } from "react-icons/hi"
@@ -6,21 +5,22 @@ import styles from "../assets/css/footer.module.css"
 import linkedin from "../assets/images/linkedIn.png"
 import twitter from "../assets/images/twitter.png"
 
-const Footer = () => {
+const Footer = (color) => {
+
     return (
         <>
-            <div className={styles.gridContainer}>
+            <div className={styles.gridContainer} style={color}>
                 <div className={styles.gridItem}>
                     <div className={styles.div1}>
                         <p> Contact Us</p>
                         <div className={styles.div1Item}>
-                            <HiOutlineMail size="20px"/>
+                            <HiOutlineMail size="20px" style={{ color: "white" }} />
                         </div>
                         <div className={styles.div1Item}>
-                            <BiSolidPhone size="20px" />
+                            <BiSolidPhone size="20px" style={{ color: "white" }} />
                         </div>
                         <div className={styles.div1Item}>
-                            <BiLogoWhatsapp size="20px" />
+                            <BiLogoWhatsapp size="20px" style={{ color: "white" }} />
                         </div>
                     </div>
                 </div>
@@ -28,7 +28,7 @@ const Footer = () => {
                     <div className={styles.div1}>
                         <p> Social Media</p>
                         <div className={styles.div1Item}>
-                            <BsFacebook size="20px"/>
+                            <BsFacebook size="20px" style={{ color: "white" }} />
                         </div>
                         <div className={styles.div1Item}>
                             <img src={linkedin} />
@@ -39,24 +39,24 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className={styles.gridItem}>
-                <div className={styles.div3}>
+                    <div className={styles.div3}>
                         <p>Legal</p>
                         <div className={styles.div3Item}>
                             <p>Terms and Services</p>
                         </div>
                         <div className={styles.div3Item}>
-                           <p> Privacy Policy</p>
+                            <p> Privacy Policy</p>
                         </div>
                         <div className={styles.div13tem}>
-                        <p> Refund Policy</p>
+                            <p> Refund Policy</p>
                         </div>
                     </div>
                 </div>
                 <div className={styles.gridItem}>
-                <div className={styles.div3}>
+                    <div className={styles.div3}>
                         <p>Support</p>
                         <div className={styles.div3Item}>
-                           <p>FAQ</p>
+                            <p>FAQ</p>
                         </div>
                     </div>
                 </div>
